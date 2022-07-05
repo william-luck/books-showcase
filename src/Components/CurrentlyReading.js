6e3046fceb08a1242283171f488320df80f85347
William Luck <luck.william.h@gmail.com>
2022-07-05 12:53:44 -0500

    add dynamic progress bar based on data obtained from json server

--- src/Components/CurrentlyReading.js
+++ src/Components/CurrentlyReading.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Card, CardGroup } from "react-bootstrap";
+import { Card, CardGroup, ProgressBar } from "react-bootstrap";
 
 function CurrentlyReading({bookReading}) {
 
-    const {title, image, author, pages, read} = bookReading
+    const {title, image, author, pages, pagesRead, read} = bookReading
 
 
     return (
@@ -17,9 +17,9 @@ function CurrentlyReading({bookReading}) {
                 <Card.Text>
                     <i>{title}</i> by {author}
                 </Card.Text>
-                {/* <Card.Text>Progress Bar</Card.Text>
-                <Card.Text>Pages, hours</Card.Text>
-                <Card.Text>Add new pages read</Card.Text> */}
+                <ProgressBar animated now={pagesRead/pages*100} />
+                {/* <Card.Text>Pages, hours</Card.Text> */}
+                {/* <Card.Text>Add new pages read</Card.Text> */}
                 </Card.Body>
                 <Card.Footer>
                 {/* <small className="text-muted">Last updated 3 mins ago</small> */}
